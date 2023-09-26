df359407867432be6ab446afa780896fe6275f82
thuantm <minhthuan1091996@gmail.com>
2023-09-26 11:15:29 +0700

    feat: add heading and labels for sliders

--- rgb_mixer.c
+++ rgb_mixer.c
@@ -1,4 +1,4 @@
-﻿/*******************************************************************************
+/*******************************************************************************
 * Title                 :   RGB mixer example with LVGL  
 * Filename              :   rgb_mixer.c
 * Author                :   ItachiVN
@@ -68,7 +68,21 @@ void slider_create(void)
 
 
 
-    //lv_obj_set_width(p_slider_r, 300);
-    //lv_obj_set_height(p_slider_r, 50);
-    //lv_obj_align(p_slider_r, LV_ALIGN_CENTER, 0, 0);
+    // Creating labels
+
+    lv_obj_t* p_heading = lv_label_create(lv_scr_act());
+    lv_label_set_text(p_heading, "RGB MIXER");
+    lv_obj_align(p_heading, LV_ALIGN_TOP_MID, 0, HEADING_Y_POS);
+
+    p_rgb_r_label = lv_label_create(lv_scr_act());
+    lv_label_set_text(p_rgb_r_label, "0");
+    lv_obj_align_to(p_rgb_r_label, p_slider_r, LV_ALIGN_OUT_TOP_MID, 0, 0);
+
+    p_rgb_g_label = lv_label_create(lv_scr_act());
+    lv_label_set_text(p_rgb_g_label, "0");
+    lv_obj_align_to(p_rgb_g_label, p_slider_g, LV_ALIGN_OUT_TOP_MID, 0, 0);
+
+    p_rgb_b_label = lv_label_create(lv_scr_act());
+    lv_label_set_text(p_rgb_b_label, "0");
+    lv_obj_align_to(p_rgb_b_label, p_slider_b, LV_ALIGN_OUT_TOP_MID, 0, 0);
 }
